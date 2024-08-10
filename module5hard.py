c2810bf254114d63beb9a86b2b1eb1b60f3c5cb7
0101Programmer <baranov-v-k@yandex.ru>
2024-08-10 16:28:07 +0300

    Add files via upload

--- module5hard.py
+++ module5hard.py
@@ -1,9 +1,12 @@
+import time
+
+
 class User:
     def __str__(self):
         return f'Логин: {self.nickname}, пароль: {self.password}, возраст: {self.age}'
 
     def __init__(self, nickname, password, age):
-        self.nickname = str(nickname)
+        self.nickname = nickname
         self.password = password
         password_h = hash(password)
         self.age = int(age)
@@ -25,49 +28,91 @@ class Video:
 
 
 class UrTube:
+    from time import sleep
 
-    def __init__(self, users=None, videos=None, current_user=User):
+    def __init__(self, users=None, videos=None, current_user=None):
         self.users = users
         if self.users is None:
-            self.users = []
+            self.users = [[]]
         self.videos = videos
         if self.videos is None:
             self.videos = []
         self.current_user = current_user
 
     def log_in(self, nickname, password):
-        if nickname in self.users and hash(password) in self.users:
-            self.current_user = nickname
+        for i in self.users:
+            if nickname in i and password in i:
+                self.current_user = nickname
+                print(f'Вход успешно выполнен, привет, {nickname}')
+            else:
+                print(f'Такого пользователя не существует')
 
     def register(self, nickname, password, age):
-        self.nickname = nickname
-        self.password = password
-        self.age = age
-        if [nickname, password, age] not in self.users:
-            self.users.append([nickname, password, age])
-            self.current_user = nickname
-        else:
-            print(f'Пользователь {nickname} уже существует')
+        for i in self.users:
+            if nickname not in i and password not in i:
+                i.append(nickname)
+                i.append(password)
+                i.append(age)
+                self.current_user = nickname
+                print(f'Пользователь {nickname} успешно зарегистрирован')
+            else:
+                print(f'Пользователь {nickname} уже существует')
+                break
 
-    def log_out(self, nickname):
-        self.nickname = nickname
+    def log_out(self):
         self.current_user = None
 
     def add(self, *args):
-        self.videos.append(args)
+        for i in args:
+            self.videos.append(i)
 
     def get_videos(self, search_word):
-        self.search_word = search_word
-        if search_word in self.videos:
-            print(self.videos)
+        get_videos_list = []
+        for i in self.videos:
+            if search_word.lower() in i.title.lower():
+                get_videos_list.append(i.title)
+        print(get_videos_list)
+
+    def watch_video(self, key_word):
+        if self.current_user is not None:
+            for i in self.videos:
+                if key_word in i.title:
+                    print(f'Воспроизводится: "{i.title}", текущая секунда просмотра:')
+                    for j in range(i.duration):
+                        print(f'\r{j:3}', end='')
+                        time.sleep(0.05)
+                    print(f'\nКонец видео')
+                    i.time_now = 0
+                    break
+                elif key_word not in i.title:
+                    print(f'Видео по запросу: "{key_word}" не найдено')
+                    break
+        elif self.current_user is None:
+            print('Войдите в аккаунт, чтобы смотреть видео')
+
 
 ur = UrTube()
-# ur.register('bab', 1234, 11)
-# ur.log_in('bab', 1234)
-# ur.log_out('bab')
+
+# работает:
+
+ur.register('Poppy', 'qwerty123', 11)
+# ur.register('Poppy', '231', 11)
+# ur.register('Molly', 'wrqo', 18)
+# ur.register('Brown', 14930, 24)
+# ur.register('Chuck', 11001, 8)
+# ur.log_in('Smith', 987654321)
+# ur.log_in('Brown', 14930)
 # print(ur.current_user)
-v1 = Video('tratata', 111)
-v2 = Video('hello word', 59)
-ur.add(v1, v2)
-print(ur.videos)
-print(v1)
+# ur.log_in('Molly', 'wrqo')
+# ur.log_out()
+# print(ur.current_user)
+
+
+v1 = Video('Белый конь', 111)
+v2 = Video('Пушистые бЕлки', 59)
+v3 = Video('Полосатый в БЕЛую полоску', 59)
+v4 = Video('Snow', 59)
+
+ur.add(v1, v2, v3, v4)
+# ur.get_videos('бел')
+ur.watch_video('Белый конь')
